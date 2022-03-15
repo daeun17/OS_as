84cea905a9ee42e21a46fa96e2db3072d3f7adaa
daeun17 <daeunk71@gmail.com>
2022-03-16 01:13:25 +0900

    2022.03.16

--- as1/test.c
+++ as1/test.c
@@ -5,7 +5,7 @@ int main(void)
 {
     int n, cpu = 0 , io = 0., i, num, j;
 
-    FILE*fp = fopen("test_files/2.inp","r");
+    FILE*fp = fopen("batch.inp","r");
     FILE*fo = fopen("batch.out","w");
 
     fscanf(fp,"%d",&n);
@@ -13,27 +13,26 @@ int main(void)
     for(i = 0; i < n ; i++)
     {
         num = 0;
-        for(j = 1; ; j++)
+        j = 1;
+        while(1)
         {
-
             fscanf(fp,"%d",&num);
-            printf("num = %d %d \n",num, j);
+            
             if(num == -1)
             {
                 break;
-            }
-
-            if( j % 2 == 0)
+            }else
             {
-                printf("io = %d \n",num);
-                io += num;
+                if( j % 2 == 0)
+                {
+                    io += num;
+                }
+                cpu += num;
             }
-            cpu += num;
-        
+            j++;
         }
 
     }
-    printf("%d %d", io, cpu);
     fprintf(fo,"%d %d",io, cpu);
 
     fclose(fp);
